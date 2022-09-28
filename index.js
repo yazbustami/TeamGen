b1d835fdfa36361b2bd7bb9fa34bea0382701dba
Yasmin Bustami <yasminbustami@Yasmins-MacBook-Pro.local>
2022-09-27 20:24:04 -0500

    Finalizing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const team = require("./SRC/team");
 const Manager = require("./Lib/Man");
 const Engineer = require("./Lib/Enj");
-const Intern = require("./Lib/Intern")
+const Intern = require("./Lib/Intern");
 
 const members = [];
 
@@ -33,6 +33,96 @@ const startManager = () => {
         .then((answers) => {
             const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
             members.push(manager);
-            menubar();
+            menu();
         })
-}
+};
+const startEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name:"name",
+            message: "what is the Engineer's name?",
+        },
+        {
+            type: "input",
+            name:"id",
+            message: "what is the Engineer's ID?",
+        },
+        {
+            type: "input",
+            name:"email",
+            message: "what is the Engineer's email?",
+        },
+        {
+            type: "input",
+            name:"github",
+            message: "what is the Engineer's Github?",
+        },
+    ])
+    .then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        members.push(engineer);
+        menu();
+    })
+};
+const startIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name:"name",
+            message: "what is the Intern's name?",
+        },
+        {
+            type: "input",
+            name:"id",
+            message: "what is the Intern's ID?",
+        },
+        {
+            type: "input",
+            name:"email",
+            message: "what is the Intern's email?",
+        },
+        {
+            type: "input",
+            name:"school",
+            message: "what is the Intern's School?",
+        },
+    ])
+    .then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        members.push(intern);
+        menu();
+    })
+};
+
+const menu = () => {
+    inquirer.prompt([
+        {
+            type:"list",
+            message: "Do you want to add another team member or creat the team?"
+            choices: ["Add an Engineer", "Add an Intern", "Create Team"],
+            name: "select",
+
+        }])
+        .then((answers) => {
+        switch(answers.select){
+            case "Add an Engineer":
+            startEngineer();
+            break;
+
+        case "Add an Intern":
+            startIntern();
+            break;
+
+        default:
+            generateTeam();
+            break;
+        }})
+};
+
+const generateTeam = () => {
+    fs.writeFile("./Dist/index.html", team(members), (err) =>
+    err ? console.log(err) : console.log ("succes!")); 
+};
+
+startManager();
